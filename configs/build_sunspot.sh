86cabb72e0b4d5f6ba5f0702827b06a8e494ba5a
TaufeqRazakh <taufeqmdr@gmail.com>
2023-10-20 19:50:15 +0000

    use frameworks and load cmake after deciding compiler in recipe

--- configs/build_sunspot.sh
+++ configs/build_sunspot.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 module load spack
+module load frameworks/2023.05.15.001
 module load cmake
-module load oneapi/eng-compiler/2023.05.15.007
- 
+
 rm -rfv build
 mkdir build
 cd build
 
 cmake \
     -DCMAKE_CXX_COMPILER=icpx \
-    -DTorch_ROOT=/soft/datascience/aurora_models_frameworks-2023.2/lib/python3.9/site-packages/torch/ \
-    -DIPEX_ROOT=/soft/datascience/aurora_models_frameworks-2023.2/lib/python3.9/site-packages/intel_extension_for_pytorch/ \
+    -DTorch_ROOT=$CONDA_PREFIX/lib/python3.9/site-packages/torch/ \
+    -DIPEX_ROOT=$CONDA_PREFIX/lib/python3.9/site-packages/intel_extension_for_pytorch/ \
     -DCMAKE_Fortran_COMPILER=mpif90 -DRXMD_ENABLE_TORCH=ON -DRXMD_ENABLE_IPEX=ON \
     .. && make
