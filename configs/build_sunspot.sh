1639da335846320a3b3165d5fcb0a62202a5527a
Ye Luo <yeluo@anl.gov>
2023-10-20 16:59:08 -0500

    Update build_sunspot.sh

--- configs/build_sunspot.sh
+++ configs/build_sunspot.sh
@@ -10,7 +10,7 @@ cd build
 
 cmake \
     -DCMAKE_CXX_COMPILER=icpx \
-    -DTorch_ROOT=$CONDA_PREFIX/lib/python3.9/site-packages/torch/ \
-    -DIPEX_ROOT=$CONDA_PREFIX/lib/python3.9/site-packages/intel_extension_for_pytorch/ \
+    -DTorch_ROOT=$CONDA_PREFIX/lib/python3.9/site-packages/torch \
+    -DIPEX_ROOT=$CONDA_PREFIX/lib/python3.9/site-packages/intel_extension_for_pytorch \
     -DCMAKE_Fortran_COMPILER=mpif90 -DRXMD_ENABLE_TORCH=ON -DRXMD_ENABLE_IPEX=ON \
     .. && make
