8e1cd4ff9959a800ef4407f6a371e083732eb4a8
Ken-ichi Nomura <knomura@usc.edu>
2024-05-01 00:14:39 +0000

    polaris 4/30 update

--- rxmdtorch/configs/build_polaris.sh
+++ rxmdtorch/configs/build_polaris.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-module unload craype-accel-nvidia80
-module load cudatoolkit-standalone/11.6.2
-module load gcc/11.2.0
+module load spack-pe-base
+module load cmake
+module load cudatoolkit-standalone cudnn cuda-PrgEnv-nvidia
 
 rm -rfv build
 mkdir build
 cd build
 
-cmake -DCMAKE_CXX_COMPILER=CC \
-  -DCMAKE_PREFIX_PATH="/soft/datascience/conda/2023-10-04/pytorch/torch;/soft/libraries/cudnn/cudnn-11-linux-x64-v8.7.0.84/" \
+cmake -DCMAKE_CXX_COMPILER=gcc-12 \
+  -DCMAKE_PREFIX_PATH="/lus/grand/projects/QuantMatManufact/knomura/libtorch" \
   .. && make
