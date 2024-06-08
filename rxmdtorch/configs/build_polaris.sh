bcfecb126b9e5286c4b561294f35657f50b7e076
Ken-ichi Nomura <knomura@usc.edu>
2024-06-08 21:50:51 +0000

    update libtorch path on Polaris

--- rxmdtorch/configs/build_polaris.sh
+++ rxmdtorch/configs/build_polaris.sh
@@ -9,5 +9,5 @@ mkdir build
 cd build
 
 cmake -DCMAKE_CXX_COMPILER=gcc-12 \
-  -DCMAKE_PREFIX_PATH="/lus/grand/projects/QuantMatManufact/knomura/libtorch" \
+  -DCMAKE_PREFIX_PATH="/lus/eagle/projects/NAQMC_RMD_aesp/knomura/libtorch" \
   .. && make
