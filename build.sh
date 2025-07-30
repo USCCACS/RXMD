bb98ddcc8d070d50959e5cd2eb0485a7c1853703
Ken-ichi Nomura <knomura@usc.edu>
2025-07-29 17:09:48 -0700

    add CARC build

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-#Polaris Config
 cmake_prefix_path=`python -c 'import torch; print(torch.utils.cmake_prefix_path)'`
+
+#Polaris Config
 sitepackage_path=`python -c 'import site; print(*site.getsitepackages())'`
 LD_LIBRARY_PATH=${sitepackage_path}/nvidia/nccl/lib:${LD_LIBRARY_PATH}
 
-#Aurora Config
-cmake_prefix_path=/lus/flare/projects/NAQMC_RMD_aesp_CNDA/knomura/libtorch-2.6.10/share/cmake
+##Aurora Config
+#cmake_prefix_path=/lus/flare/projects/NAQMC_RMD_aesp_CNDA/knomura/libtorch-2.6.10/share/cmake
+
+##CARC
+#module purge
+#module load usc openblas gcc cuda cmake openmpi cudnn intel-oneapi-mkl
+#module unload python
+
 
 #Common Part
 echo $cmake_prefix_path
